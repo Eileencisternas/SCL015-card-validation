f589028cfea153f9a2382728ec6b5a0672d99f29
Eileencisternas <eileencisternasr@gmail.com>
2020-11-16 08:32:55 -0300

    correccion español a ingles js

--- src/validator.js
+++ src/validator.js
@@ -35,13 +35,13 @@ const validator = {
     //si el long de la tarjeta es menor a 6 caracteres devuelvo el mismo numero ingresado
     if (creditCardNumber.length < 6) return creditCardNumber;
     //almaceno los ultimos 4 digitos//substr retorno una parte del string
-    let ultimos4digitos = creditCardNumber.substr(-4);
+    let last4digit = creditCardNumber.substr(-4);
     //tomo todos los caracteres a excepcion de los ultimos 4 digitos
-    let caracteresaremplazar = creditCardNumber.substr(0,creditCardNumber.length - 4);
+    let replacecharacter = creditCardNumber.substr(0,creditCardNumber.length - 4);
     //tomo los numeros anteriores distintos de los ultimos 4 y los reemplazo por #
-  let ocultarcaracter = caracteresaremplazar.replace(/\w/g, "#");//(/\w/g) expresion regular para reemplazar todos los valores del string
+  let hidecharacter = replacecharacter.replace(/\w/g, "#");//(/\w/g) expresion regular para reemplazar todos los valores del string
     //y por ultimo retorno la union de los caracteres de la variable ocultar caracteres y los ultimos 4 digitos
-    return `${ocultarcaracter}${ultimos4digitos}`;
+    return `${hidecharacter}${last4digit}`;
   },
 };
 
